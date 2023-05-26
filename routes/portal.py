94a0e3ba2a28a116365b7652e81afd58975c78f7
joel1844 <friascastillo0403@gmail.com>
2023-05-26 09:08:46 -0400

    NRES

--- routes/portal.py
+++ routes/portal.py
@@ -48,7 +48,7 @@ async def create_user(req:Request ,portal: Portal = Depends(), video_file: Optio
         with open(video_rutacompleta, 'wb') as f:
             f.write(video_content)
         
-        guardarvideo = f"{req.base_url}archivo/{video_id}"
+        guardarvideo = f"{req.base_url}VIOLENCIA/{video_id}"
     
     image_id = None
     guardarimagen = None
@@ -60,7 +60,7 @@ async def create_user(req:Request ,portal: Portal = Depends(), video_file: Optio
         with open(image_rutacompleta, 'wb') as f:
             f.write(image_content)
 
-        guardarimagen = f"{req.base_url}archivo/{image_id}"
+        guardarimagen = f"{req.base_url}VIOLENCIA/{image_id}"
 
     new_portal = {
         "name": portal.name,
@@ -88,34 +88,6 @@ async def create_user(req:Request ,portal: Portal = Depends(), video_file: Optio
 
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=portalEntity(portal))
 
-    # video_content = await video_file.read() # leer el contenido del archivo
-    # image_content = await image_file.read()  # leer el contenido del archivo de imagen
-
-    # carpeta = ("VIOLENCIA")
-    # contenido_archivo = video_content
-    # video_id = colletionvideo.insert_one({"description": video_file.filename}).inserted_id
-    # image_id = colletionvideo.insert_one({"description": image_file.filename}).inserted_id
-    # os.makedirs(carpeta, exist_ok=True)
-
-    # rutacompleta = os.path.join(carpeta, f"{str(video_id)}.{video_file.content_type.split('/'   )[1]}")
-    # guardarvideo = f"{req.base_url}archivo/{video_id}"
-    
-
-    # with open(rutacompleta, 'wb') as f:
-    #     print(rutacompleta)
-    #     # escribir los datos binarios en el archivo
-    #     f.write(contenido_archivo)
-    
-    # f.close() #
-
-    # new_portal = {"name": portal.name, "date": portal.date, "video": guardarvideo, "latitude": portal.latitude, "longitude": portal.longitude,"clasificacion": portal.clasification, "descripcion": portal.description, "status": "Pendiente"}
-    # # del new_portal["id"]
-    # id = collectionportal.insert_one(new_portal)
-    # portal =  collectionportal.find_one({"_id": id.inserted_id})
-
-    # return JSONResponse(status_code=status.HTTP_201_CREATED, content=portalEntity(portal))
-
-
 
 @portal.get("/instagraminfo/", tags=["portal"])
 def find_all_users2():
@@ -123,55 +95,55 @@ def find_all_users2():
     return instagramEsEntity(collentioninsta.find())
 
 
-@portal.post("/instagraminfo/", tags=["portal"])
-async def create_user2(url: str):
-    response = requests.get(url)
-    html = response.text
-    L = instaloader.Instaloader()
-    # Encuentra el enlace de descarga del video y la información del post
-    shortcode = url.split("/")[-2]
-    post = instaloader.Post.from_shortcode(L.context, shortcode)
-    video_url = url
-    title = post.caption
-    date = post.date,
-    imagen = post.url
+# @portal.post("/instagraminfo/", tags=["portal"])
+# async def create_user2(url: str):
+#     response = requests.get(url)
+#     html = response.text
+#     L = instaloader.Instaloader()
+#     # Encuentra el enlace de descarga del video y la información del post
+#     shortcode = url.split("/")[-2]
+#     post = instaloader.Post.from_shortcode(L.context, shortcode)
+#     video_url = url
+#     title = post.caption
+#     date = post.date,
+#     imagen = post.url
 
-    date  = date.strftime("%d/%m/%Y")
-    owner_username = post.owner_username
+#     date  = date.strftime("%d/%m/%Y")
+#     owner_username = post.owner_username
 
 
-    new_scrape = {"Nombre": title, "fecha": str(date), "video": video_url, "owner_username": owner_username, "status": "Pendiente", 'fuente': 'instagram', "imagen": imagen}
-    # del new_portal["id"]
-    id = collentioninsta.insert_one(new_scrape)
-    new_scrape =  collentioninsta.find_one({"_id": id.inserted_id})
+#     new_scrape = {"Nombre": title, "fecha": str(date), "video": video_url, "owner_username": owner_username, "status": "Pendiente", 'fuente': 'instagram', "imagen": imagen}
+#     # del new_portal["id"]
+#     id = collentioninsta.insert_one(new_scrape)
+#     new_scrape =  collentioninsta.find_one({"_id": id.inserted_id})
     
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=instagramEntity(new_scrape))
+#     return JSONResponse(status_code=status.HTTP_201_CREATED, content=instagramEntity(new_scrape))
 
-@portal.post("/youtubeinfo/", tags=["portal"])
-async def create_user3(url: str):
-# Create a PyTube YouTube object
-    yt = pytube.YouTube(url)
+# @portal.post("/youtubeinfo/", tags=["portal"])
+# async def create_user3(url: str):
+# # Create a PyTube YouTube object
+#     yt = pytube.YouTube(url)
 
-    # Get the video title and publish date
-    video_title = yt.title
-    video_publish_date = yt.publish_date
-    video_publish_date = video_publish_date.strftime("%d/%m/%Y")
-    owner_username = yt.author
+#     # Get the video title and publish date
+#     video_title = yt.title
+#     video_publish_date = yt.publish_date
+#     video_publish_date = video_publish_date.strftime("%d/%m/%Y")
+#     owner_username = yt.author
 
-    # Check if the video is a stream or an adaptive stream
-    if yt.streams.first().type == "video":
-        # Video stream
-        video_url = yt.streams.first().url
-    else:
-        # Adaptive stream
-        video_url = yt.streams.filter(progressive=True).order_by('resolution').desc().first().url
+#     # Check if the video is a stream or an adaptive stream
+#     if yt.streams.first().type == "video":
+#         # Video stream
+#         video_url = yt.streams.first().url
+#     else:
+#         # Adaptive stream
+#         video_url = yt.streams.filter(progressive=True).order_by('resolution').desc().first().url
 
-    new_scrape = {"Nombre": video_title, "fecha": str(video_publish_date), "video": video_url, "owner_username": owner_username, "status": "Pendiente", 'fuente': 'youtube', "imagen": "https://www.youtube.com/img/desktop/yt_1200.png"}
-    # del new_portal["id"]
-    id = collentioninsta.insert_one(new_scrape)
-    new_scrape =  collentioninsta.find_one({"_id": id.inserted_id})
+#     new_scrape = {"Nombre": video_title, "fecha": str(video_publish_date), "video": video_url, "owner_username": owner_username, "status": "Pendiente", 'fuente': 'youtube', "imagen": "https://www.youtube.com/img/desktop/yt_1200.png"}
+#     # del new_portal["id"]
+#     id = collentioninsta.insert_one(new_scrape)
+#     new_scrape =  collentioninsta.find_one({"_id": id.inserted_id})
 
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=instagramEntity(new_scrape))
+#     return JSONResponse(status_code=status.HTTP_201_CREATED, content=instagramEntity(new_scrape))
 
 
 #make scrape diario libre 
@@ -185,6 +157,11 @@ def find_all_users3():
 @portal.get("/archivo/{nombre_archivo}")
 async def get_archivo(nombre_archivo: str):
     return FileResponse(f"VIOLENCIA/{nombre_archivo}.mp4")
+
+#listar todos los videos que vienen de la carpeta VIOLENCIA 
+@portal.get("/listarvideos/", tags=["portal"])
+def find_all_users4():
+    return listvideosEntity(collentionvideo.find())
 
 
 
